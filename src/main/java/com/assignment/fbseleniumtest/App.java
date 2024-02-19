067c4890e1b39c8d48d7ca2a4847841b823fdb9c
amitava9018 <91630288+amitava9018@users.noreply.github.com>
2024-02-19 17:07:06 -0500

    Update App.java

--- src/main/java/com/assignment/fbseleniumtest/App.java
+++ src/main/java/com/assignment/fbseleniumtest/App.java
@@ -19,35 +19,37 @@ public class App
 	public static void main( String[] args ) throws InterruptedException
     {
     	//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    	WebDriverManager.chromedriver().setup();
-    	
-        WebDriver driver=new ChromeDriver();
-        ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--window-size=1920,1080");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        System.out.println( "Automation script is executing" );
-        
-        //Implicit wait
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize(); 
-        driver.get("https://www.facebook.com/");
-        System.out.println("FB Site opened");
-        
-        WebElement userEmail = driver.findElement(By.id("email"));
-        userEmail.sendKeys("test123@email.com");
-        System.out.println("email entered");
-        
-        WebElement password = driver.findElement(By.id("pass"));
-        password.sendKeys("Test@123");
-        System.out.println("password entered");
-        
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        WebElement logInButton = driver.findElement(By.name("login"));
-        logInButton.click();
-        System.out.println("Log in button clicked");
 
-        driver.quit();
-        System.out.println("script executed successfully");
+	WebDriverManager.chromedriver().setup(); 
+    	ChromeOptions chromeOptions = new ChromeOptions(); 
+    	chromeOptions.addArguments("--headless");
+    	  
+    	WebDriver driver = new ChromeDriver(chromeOptions);
+    	  
+        System.out.println("Scripted Executing");
+    	  
+    	  //1. Open the browser and get to the url
+    	  driver.get("https://www.facebook.com/");
+    	  System.out.println("FB Site opened");
+	    
+    	  //enable wait of 3 seconds 
+    	  driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+    	  
+    	  //Locate user name and enter details.
+    	  driver.findElement(By.id("email")).sendKeys("test123@email.com");
+    	  System.out.println("email entered");
+    	  
+    	  
+    	  //locate password and enter details.
+    	  driver.findElement(By.id("pass")).sendKeys("Test@123");
+    	  System.out.println("password entered");
+    	  
+    	  //Locate Login button and click on save
+    	  driver.findElement(By.name("login")).click();
+    	  System.out.println("Log in button clicked");
+	    
+    	  Thread.sleep(1000);
+    	  
+    	  System.out.println("Scripted executed successfully"); Thread.sleep(1000); 
     }
 }
